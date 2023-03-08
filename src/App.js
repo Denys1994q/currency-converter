c338747577e4b9b9aa260398adb80cd831654e9b
Denys1994q <drybachok@gmail.com>
2023-03-08 21:13:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CurrencyChanger from './components/currency/currencyChanger/CurrencyChanger'
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyChanger />
+    </div>
+  );
+}
+
+export default App;
